4bbab495d21da0e92c57cc79a8e1f43abe63029e
igoryen <igor.yentaltsev@gmail.com>
2015-01-27 15:56:37 -0500

    Album/Module.php: created

---
+++ module/Album/Module.php
@@ -0,0 +1,29 @@
+<?php
+// 1
+
+namespace Album;
+
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface {
+  // 2
+  public function getAutoloaderConfig() {
+    return array(
+        'Zend\Loader\ClassMapAutoloader' => array(
+            __DIR__ . '/autoload_classmap.php',
+        ),
+        'Zend\Loader\StandardAutoloader' => array(
+            'namespaces' => array(
+                __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+            ),
+        ),
+    );
+  }
+  
+  // 3
+  public function getConfig() {
+    return include __DIR__ . '/config/module.config.php';
+  }
+
+}
