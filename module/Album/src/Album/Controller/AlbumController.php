40a6c4ffca71dd1c9d55bb0824dc09685741aaa7
igoryen <igor.yentaltsev@gmail.com>
2015-02-02 15:28:20 -0500

    bug5 - Form to add new album: displayed

--- module/Album/src/Album/Controller/AlbumController.php
+++ module/Album/src/Album/Controller/AlbumController.php
@@ -4,6 +4,8 @@ namespace Album\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Album\Model\Album;
+use Album\Form\AlbumForm;
 
 class AlbumController extends AbstractActionController {
   
@@ -19,7 +21,24 @@ class AlbumController extends AbstractActionController {
 
   // GET: /album/add
   public function addAction() {
-    
+    $form = new AlbumForm(); // 56
+    $form->get('submit')->setValue('Add'); // 57
+
+    $request = $this->getRequest();
+    if ($request->isPost()) { // 58
+      $album = new Album();
+      $form->setInputFilter($album->getInputFilter()); // 59
+      $form->setData($request->getPost()); // 60
+
+      if ($form->isValid()) { // 61
+        $album->exchangeArray($form->getData()); // 62
+        $this->getAlbumTable()->saveAlbum($album); // 63
+
+        // Redirect to list of albums
+        return $this->redirect()->toRoute('album'); // 64
+      }
+    }
+    return array('form' => $form); // 65
   }
   // GET: /album/edit
   public function editAction() {
--- module/Album/src/Album/Model/Album.php
+++ module/Album/src/Album/Model/Album.php
@@ -1,18 +1,90 @@
 <?php
 
 namespace Album\Model;
+
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface; // 44
+use Zend\InputFilter\InputFilterInterface;
+
 // 13
-class Album {
+class Album implements InputFilterAwareInterface { // 44
 
   public $id;
   public $artist;
   public $title;
+  protected $inputFilter;  
 
   // 14
   public function exchangeArray($data) {
     $this->id = (!empty($data['id'])) ? $data['id'] : null;
     $this->artist = (!empty($data['artist'])) ? $data['artist'] : null;
     $this->title = (!empty($data['title'])) ? $data['title'] : null;
+  }
+  
+  // 45
+  public function setInputFilter(InputFilterInterface $inputFilter) {
+    throw new \Exception("Not used"); // 47
+  }
+
+  // 46
+  public function getInputFilter() {
+    if (!$this->inputFilter) {
+      $inputFilter = new InputFilter(); // 48
+
+      // 49 
+      
+      // 50
+      $inputFilter->add(array(
+        'name' => 'id',
+        'required' => true,
+        'filters' => array(
+          array('name' => 'Int'),
+        ),
+      ));
+
+      // 51
+      $inputFilter->add(array(
+        'name' => 'artist',
+        'required' => true, // 52
+        'filters' => array(
+          array('name' => 'StripTags'), // 54
+          array('name' => 'StringTrim'), // 55
+        ),
+        'validators' => array(
+          array(
+            'name' => 'StringLength', // 53
+            'options' => array(
+              'encoding' => 'UTF-8',
+              'min' => 1,
+              'max' => 100,
+            ),
+          ),
+        ),
+      ));
+
+      $inputFilter->add(array(
+        'name' => 'title',
+        'required' => true,
+        'filters' => array(
+          array('name' => 'StripTags'),
+          array('name' => 'StringTrim'),
+        ),
+        'validators' => array(
+          array(
+            'name' => 'StringLength',
+            'options' => array(
+              'encoding' => 'UTF-8',
+              'min' => 1,
+              'max' => 100,
+            ),
+          ),
+        ),
+      ));
+
+      $this->inputFilter = $inputFilter;
+    }
+
+    return $this->inputFilter;
   }
 
 }
