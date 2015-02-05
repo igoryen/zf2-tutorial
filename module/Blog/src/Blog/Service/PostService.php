52c938f146ca40702efb6e8e87d7e02ee0ef18b3
igoryen <igor.yentaltsev@gmail.com>
2015-02-05 09:51:42 -0500

    bug15 - Bringing Life into our PostService

--- module/Blog/src/Blog/Service/PostService.php
+++ module/Blog/src/Blog/Service/PostService.php
@@ -4,21 +4,63 @@
 
 namespace Blog\Service;
 
-// 11
+// 115
 class PostService implements PostServiceInterface {
+
+  // 117
+  protected $data = array(
+    array(
+      'id' => 1,
+      'title' => 'Hello World #1',
+      'text' => 'This is our first blog post!'
+    ),
+    array(
+      'id' => 2,
+      'title' => 'Hello World #2',
+      'text' => 'This is our second blog post!'
+    ),
+    array(
+      'id' => 3,
+      'title' => 'Hello World #3',
+      'text' => 'This is our third blog post!'
+    ),
+    array(
+      'id' => 4,
+      'title' => 'Hello World #4',
+      'text' => 'This is our fourth blog post!'
+    ),
+    array(
+      'id' => 5,
+      'title' => 'Hello World #5',
+      'text' => 'This is our fifth blog post!'
+    )
+  );
 
   /**
    * {@inheritDoc}
    */
   public function findAllPosts() {
-// TODO: Implement findAllPosts() method.
+    $allPosts = array();
+
+    foreach ($this->data as $index => $post) {
+      $allPosts[] = $this->findPost($index); // 118
+    }
+
+    return $allPosts;
   }
 
   /**
    * {@inheritDoc}
    */
   public function findPost($id) {
-// TODO: Implement findPost() method.
+    $postData = $this->data[$id];
+
+    $model = new Post();
+    $model->setId($postData['id']);
+    $model->setTitle($postData['title']);
+    $model->setText($postData['text']);
+
+    return $model;
   }
 
 }
